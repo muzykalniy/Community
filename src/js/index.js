520cee0f5cfa13af8e553150d512bfadac8c78ca
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-06-08 14:58:09 +0200

    commit slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,29 @@
-// Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// import styles bundle
+import "swiper/css/bundle";
+
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+
+  slidesPerView: 1, // Отображает 1 слайд одновременно
+  slidesPerGroup: 1, // Переключает по 1 слайду за раз
+  loop: true, // Позволяет бесконечную прокрутку
+  spaceBetween: 30, // Расстояние между слайдами (по желанию)
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
