f4c2bd77088aae1633dc5926f3219d295c203af5
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-06-09 18:54:37 +0200

    page transition

--- src/js/index.js
+++ src/js/index.js
@@ -24,6 +24,34 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30, // Расстояние между слайдами (по желанию)
 });
 
+// бесшовный переход между страницами вьюпорта
+let currentPage = 1;
+let isTransitioning = false;
+
+function goToPage(pageNumber) {
+  if (isTransitioning || pageNumber === currentPage) return;
+  const container = document.getElementById("container__transition");
+  if (pageNumber === 1) {
+    container.style.transform = "translateY(0)";
+  } else {
+    container.style.transform = "translateY(-50%)";
+  }
+  currentPage = pageNumber;
+  isTransitioning = true;
+  setTimeout(() => {
+    isTransitioning = false;
+  }, 500); // Duration of the transition
+}
+
+window.addEventListener("wheel", (event) => {
+  if (isTransitioning) return;
+  if (event.deltaY > 0) {
+    goToPage(2);
+  } else {
+    goToPage(1);
+  }
+});
+
 // Мобильная навигация
 import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
