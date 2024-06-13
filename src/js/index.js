b21080e77c33c301f9c0b6938d99ed95c3208fe0
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-06-13 18:43:12 +0200

    swiper features resp

--- src/js/index.js
+++ src/js/index.js
@@ -23,20 +23,43 @@ var swiper = new Swiper(".swiper__hero", {
   loop: true, // Позволяет бесконечную прокрутку
   spaceBetween: 30, // Расстояние между слайдами (по желанию)
 });
+// swiper features с учетом изменения размера экрана
 
-var swiper = new Swiper(".swiper__features", {
-  direction: "vertical",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  mousewheel: false,
-  keyboard: true,
+let swiperInstance;
 
-  slidesPerView: 1, // Отображает 1 слайд одновременно
-  slidesPerGroup: 1, // Переключает по 1 слайду за раз
-  loop: true, // Позволяет бесконечную прокрутку
-  spaceBetween: 100, // Расстояние между слайдами (по желанию)
+function initializeSwiper() {
+  // Если экземпляр Swiper уже существует, уничтожаем его перед созданием нового
+  if (swiperInstance) {
+    swiperInstance.destroy(true, true);
+  }
+
+  // Определение направления Swiper в зависимости от ширины окна
+  const direction = window.innerWidth < 600 ? "horizontal" : "vertical";
+
+  // Инициализация Swiper с новыми параметрами
+  swiperInstance = new Swiper(".swiper__features", {
+    direction: direction,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    mousewheel: false,
+    keyboard: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    spaceBetween: 300,
+  });
+}
+
+// Проверка начальной конфигурации
+initializeSwiper();
+
+// Слушатель для изменения размера окна с задержкой для предотвращения частых вызовов
+let resizeTimeout;
+window.addEventListener("resize", () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(initializeSwiper, 300);
 });
 
 // Мобильная навигация
